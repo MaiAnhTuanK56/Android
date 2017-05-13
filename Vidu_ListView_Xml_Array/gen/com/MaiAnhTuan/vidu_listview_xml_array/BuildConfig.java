ba2b2458c6bf28904480cf884563559373cc4b91
anhtuank56 <protopvip123@gmail.com>
2017-05-13 22:45:24 +0700

    Revert "Bài tập 13 -Thực hành về ListView trong Android"
    This reverts commit a7d029ca222a6a0466a349bfe209492b769a2471.

--- Vidu_ListView_Xml_Array/gen/com/MaiAnhTuan/vidu_listview_xml_array/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.MaiAnhTuan.vidu_listview_xml_array;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
