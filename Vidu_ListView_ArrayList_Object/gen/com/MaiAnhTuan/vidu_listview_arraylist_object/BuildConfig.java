ee9cb8d7abcf691f37b2e2d55cb82b2496f81764
anhtuank56 <protopvip123@gmail.com>
2017-05-15 11:44:42 +0700

    Bài tập 13 -Thực hành về ListView trong Android

---
+++ Vidu_ListView_ArrayList_Object/gen/com/MaiAnhTuan/vidu_listview_arraylist_object/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.MaiAnhTuan.vidu_listview_arraylist_object;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Vidu_ListView_Xml_Array/gen/com/MaiAnhTuan/vidu_listview_xml_array/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.MaiAnhTuan.vidu_listview_xml_array;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
