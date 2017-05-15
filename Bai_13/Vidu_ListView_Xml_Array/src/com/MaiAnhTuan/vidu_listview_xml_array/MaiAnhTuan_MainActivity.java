1dac32586e5717aa1d890a0217fa56da4be454a8
anhtuank56 <protopvip123@gmail.com>
2017-05-15 11:54:48 +0700

    Bài tập 13 -Thực hành về ListView trong Android

---
+++ Bai_13/Vidu_ListView_Xml_Array/src/com/MaiAnhTuan/vidu_listview_xml_array/MaiAnhTuan_MainActivity.java
@@ -0,0 +1,34 @@
+package com.MaiAnhTuan.vidu_listview_xml_array;
+
+
+	import android.os.Bundle;
+	import android.app.Activity;
+	import android.view.View;
+	import android.widget.AdapterView;
+	import android.widget.ArrayAdapter;
+	import android.widget.ListView;
+	import android.widget.TextView;
+
+	public class MaiAnhTuan_MainActivity extends Activity {
+
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_mai_anh_tuan__main);
+			final String arr[]=getResources().getStringArray(R.array.myarry);
+			ListView lv=(ListView) findViewById(R.id.lvperson);
+			ArrayAdapter<String>adapter=new ArrayAdapter<String>
+				(this, android.R.layout.simple_list_item_1, arr);
+			lv.setAdapter(adapter);
+			final TextView txt=(TextView) findViewById(R.id.txtselection);
+			lv.setOnItemClickListener(
+					new AdapterView.OnItemClickListener() {
+						public void onItemClick(AdapterView<?> arg0,
+								View arg1, 
+								int arg2,
+								long arg3) {
+							txt.setText("position :"+arg2+" ; value ="+arr[arg2]);
+						}
+					});
+		}
+	}
+
